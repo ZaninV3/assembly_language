1b9badd4995e164b7dfc1ccd60c620e023da49c2
ZaninV3 <zanin03.03@mail.ru>
2024-01-09 23:03:27 +0900

    Решено задание #2
    Весь код перенесен в отдельную функцию. Код работает корректно.

--- loops_and_arrays/loops_and_arrays/loops_and_arrays.cpp
+++ loops_and_arrays/loops_and_arrays/loops_and_arrays.cpp
@@ -1,65 +1,79 @@
 ﻿// Занин Вячеслав ВМК-21
-#include <iostream>  // Для ввода/вывода в консоль
+#define general_type unsigned int
+#include <iostream>
+using namespace std;
 
-// Здесь начало работы программы
+// Функция, внутри которой выполняется основная работа программы
+general_type countEvenSequence(general_type n, general_type* arr) {
+    // Инициализируем переменнную-счетчик
+    general_type count = 0;
+
+    /*
+    // Решим на C++
+    for (general_type i = 0; i < n; i++) {
+        // В задании указано, что первый элемент имеет индекс 1
+        if ((i + 1) % 2 != 0 and arr[i] % 2 == 0) count++;
+    }
+    */
+
+    __asm {
+        mov ecx, n           // Загрузка количества элементов
+        mov ebx, 0           // Инициализация счетчика для второго цикла
+        mov esi, arr         // Загрузка адреса массива
+        mov eax, 0           // Обнуление регистра eax для подсчета количества четных чисел
+
+        loop_start:
+            add ebx, 1
+            mov edx, [esi]      // Загрузка значения текущего элемента
+            test edx, 1         // Проверка на четность текущего элемента
+            jnz next_e          // Если нечетное, переходим к следующему элементу
+            test edx, 1         // Проверка на четность текущего элемента
+            jz increment        // Если четное, продолжаем проверку
+        increment:
+            mov edx, ebx        // Загружаем индекс массива
+            test edx, 1         // Проверка на четность индекса
+            jnz next_i          // Если нечетное, то 
+            test edx, 1         // Проверка на четность индекса
+            jz next_e           // Увеличиваем счетчик
+        next_i:
+            inc eax             // Увеличение счетчика
+        next_e:
+            add esi, 4          // Увеличение указателя на размер элемента (4 байта)
+            loop loop_start     // Переход к следующему элементу
+
+        mov count, eax      // Сохранение результата в переменную count
+    }
+    return count;
+}
+
 int main() {
-	/*
-		Дано натуральные n и a1, a2, ... an.
-		Определить количество последовательности ak
-		имеющих нечётные порядковые номера и являющимися
-		чётными числами.
 
-		Например: [4, 3, 56, 32, 35] -> 2
+    /*
+        Дано натуральные n и a1, a2, ... an.
+        Определить количество последовательности ak
+        имеющих нечётные порядковые номера и являющимися
+        чётными числами.
 
-		Вопрос с 0 возникать не должен,
-		так как он по задаче нами может не предусматриваться.
-	*/
+        Например: [4, 3, 56, 32, 35] -> 2
 
-	// Введем n
-	unsigned short n;
-	std::cout << "enter n: ";
-	std::cin >> n;
+        Вопрос с 0 возникать не должен,
+        так как он по задаче нами может не предусматриваться.
+    */
 
-	// Сделаем указатель на ячейку памяти для обращения к элементам
-	unsigned short* input_massive = new unsigned short[n];
+    // Вводим n
+    general_type n;
+    cout << "enter n: ";
+    cin >> n;
 
-	// Поскольку нам нужно ввести значения массива
-	// То здесь нам как кровь из носа нужен высокоуровневый for
-	for (unsigned short i = 0; i < n; i++) {
-		std::cout << "enter a" << i + 1 << ": ";
-		std::cin >> input_massive[i];
-	}
-	// Сделаем переменную-счётчик
-	unsigned short count;
+    // Вводим значения массива
+    general_type *arr = new general_type[n];
+    for (general_type i = 0; i < n; i++) {
+        cout << "enter a" << i + 1 << ": ";
+        cin >> arr[i];
+    }
 
-	// ???
-	// https://www.cyberforum.ru/cpp-beginners/thread2159053.html
-	__asm {
-		mov esi, arr
-		add esi, 4
-		mov ecx, n
-		mov ebx, 2
-		next_1:
-			mov eax, [esi]
-			imul ebx
-			mov[esi], eax
-			add esi, 8
-		loop next_1
-	}
+    // Подсчет и вывод результата
+    cout << "the count of values is " << countEvenSequence(n, arr) << endl;
 
-	/*
-	// Решим на C++
-
-	// Проинициализируем 0;
-	count = 0;
-
-	// По условию нам дан n,
-	// поэтому имеем право его использовать
-	for (unsigned short i = 0; i < n; i++) {
-		// В задании указано, что первый элемент имеет индекс 1
-		if ((i + 1) % 2 != 0 and input_massive[i] % 2 == 0) count++;
-	}
-	*/
-	// Ну а вот и вывод
-	std::cout << "the count of values is " << count;
+    delete[] arr;
 }
